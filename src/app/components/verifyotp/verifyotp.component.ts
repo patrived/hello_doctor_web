0d6c523c1c71504b206fe2613028f881f47f62fd
Palash Trivedi <thinkert26@gmail.com>
2025-02-20 14:51:05 +0530

    verfy opt changes done

---
+++ src/app/components/verifyotp/verifyotp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verifyotp',
+  templateUrl: './verifyotp.component.html',
+  styleUrls: ['./verifyotp.component.css']
+})
+export class VerifyotpComponent {
+
+}
